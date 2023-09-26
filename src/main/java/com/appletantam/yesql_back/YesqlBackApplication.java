0642daa27ae121f74ad472e50a8f1c7351e862f6
banxzxx <jwbang1643@gmail.com>
2023-09-26 22:51:29 +0900

    init commit

---
+++ src/main/java/com/appletantam/yesql_back/YesqlBackApplication.java
@@ -0,0 +1,13 @@
+package com.appletantam.yesql_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesqlBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesqlBackApplication.class, args);
+	}
+
+}
