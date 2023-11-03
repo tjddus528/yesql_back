4131055271ef0deeff43412fb00b0204dde9f234
sungyeon <csy357@gmail.com>
2023-11-03 19:48:25 +0900

    Feat: antlr api 호출 테스트

--- src/main/java/com/appletantam/yesql_back/TestController.java
+++ src/main/java/com/appletantam/yesql_back/TestController.java
@@ -1,13 +1,27 @@
 package com.appletantam.yesql_back;
 
+import com.appletantam.yesql_back.config.antlr.AntlrService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/manage")
 public class TestController {
 
-    @GetMapping("/sig")
-    public String signUp() {
-        return "done.";
+    @Autowired
+    AntlrService antlrService;
+    @GetMapping("/antlr")
+    public ArrayList antlr() throws IOException {
+        ArrayList arrayList = antlrService.getData("http://localhost:9000/antlr/run?sql=SELECT loan_number FROM borrower WHERE customer_name = (SELECT customer_name FROM depositor WHERE account_number = \"A-215\");");
+        return arrayList;
     }
+
 }
